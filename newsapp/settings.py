6b01b7a3fc753703675f0542139aafead84ed93a
sakshiandhale12 <sakshiandhale12@gmail.com>
2023-08-30 17:51:57 +0530

    initial commit

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-slt4vmooi%&$hoy#*@r%-f)-b+emyzqrd8e3u065bs^f^^1v7i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -117,11 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavsaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
