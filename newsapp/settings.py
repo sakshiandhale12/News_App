4f6d687cdffd8748c0cf0ff7f3eefaa40d614277
sakshiandhale12 <sakshiandhale12@gmail.com>
2023-08-30 13:12:30 +0530

    initial commit

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -115,7 +115,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavsaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
